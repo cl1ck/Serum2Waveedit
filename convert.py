5319030305cdfab4e02bd502bc9a69a40fb97ab6
Michel Hofmann <cl1ck@cl1ck0ne.net>
2020-02-02 19:23:15 +0100

    fixed 16bit conversion

--- convert.py
+++ convert.py
@@ -1,4 +1,4 @@
-from osc_gen import wavetable, dsp, sig, visualize, wavfile
+from osc_gen import wavetable, visualize
 import scipy.io.wavfile
 import scipy.signal
 import numpy as np
@@ -6,6 +6,7 @@ import os
 import io
 import struct
 import sys
+import soundfile as sf
 
 SAMPLE_RATE = 44100
 SERUM_WAVE_LENGTH = 2048
@@ -48,19 +49,19 @@ def save_as_waveedit(waves, filename):
     table.to_wav(filename, SAMPLE_RATE)
 
 
-def convert_wav_to_16bit(input_filename):
+def convert_wav_to_table(input_filename):
     filename, file_extension = os.path.splitext(input_filename)
     temp_filename = filename + '_16bit' + file_extension
 
-    rate, serum_wav = scipy.io.wavfile.read(input_filename)
+    serum_wav, samplerate = sf.read(input_filename)
+    if os.path.isfile(temp_filename):
+        os.remove(temp_filename)
+    sf.write(temp_filename, serum_wav, SAMPLE_RATE, subtype='PCM_16')
+
     clm_header = read_clm_header(input_filename)
     wave_len = int(clm_header[3:7])
     num_slots = int(len(serum_wav) / wave_len)
-    wav16 = np.int16(np.clip(serum_wav, -1, 1) * AMPLITUDE)
 
-    if os.path.isfile(temp_filename):
-        os.remove(temp_filename)
-    wavfile.write(wav16, temp_filename, SAMPLE_RATE)
     table = wavetable.WaveTable(num_slots, wave_len=wave_len).from_wav(temp_filename, resynthesize=False)
     os.remove(temp_filename)
 
@@ -69,7 +70,7 @@ def convert_wav_to_16bit(input_filename):
 
 def convert_serum_to_waveedit(serum_filename):
     # get 64 waves evenly selected from the input wavetable
-    input_table = convert_wav_to_16bit(serum_filename)
+    input_table = convert_wav_to_table(serum_filename)
     idx = np.round(np.linspace(0, len(input_table.waves) - 1, WAVEEDIT_NUM_SLOTS)).astype(np.int16)
     selected_waves = list(np.array(input_table.waves)[idx])
 
@@ -101,8 +102,8 @@ def main():
         print("Usage: {0} SERUM_WAV_FILE".format(sys.argv[0]))
         exit()
 
-    converted_filename = convert_serum_to_waveedit(sys.argv[1])
-    plot_waveedit_file(converted_filename)
+    convert_serum_to_waveedit(sys.argv[1])
+    # plot_waveedit_file(converted_filename)
 
 
 if __name__ == '__main__':
